b429f2f51ae0cca274c86e66f5343201a6ba5082
TangWang <2606176542@qq.COM>
2019-05-28 15:13:27 +0800

    Delete game_mountCar.py

--- game_mountCar.py
+++
@@ -1,8 +0,0 @@
-import gym
-# env = gym.make('MountainCar-v0')
-env = gym.make('MsPacman-v0')
-env.reset()
-for _ in range(1000):
-    env.render() #输出游戏场景信息
-    env.step(env.action_space.sample()) # take a random action
-env.close()
